c904ec16118e6372013c0126af9f1adea38fd04c
muiz332 <muizzuddin332@gmail.com>
2023-05-08 18:48:39 +0700

    add cors

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-import express from 'express'
-import roles from './routes/roles.js'
-import users from './routes/users.js'
-import katPengaduan from './routes/kategoriPengaduan.js'
-import gambar from './routes/gambar.js'
-import pengaduan from './routes/pengaduan.js'
-import krisar from './routes/krisar.js'
-import morgan from 'morgan'
+import express from "express";
+import roles from "./routes/roles.js";
+import users from "./routes/users.js";
+import katPengaduan from "./routes/kategoriPengaduan.js";
+import gambar from "./routes/gambar.js";
+import pengaduan from "./routes/pengaduan.js";
+import krisar from "./routes/krisar.js";
+import morgan from "morgan";
+import cors from "cors";
 
-const app = express()
-app.use(express.json())
-app.use(morgan("tiny"))
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
 
+app.use("/roles", roles);
+app.use("/users", users);
+app.use("/kategori-pengaduan", katPengaduan);
+app.use("/pengaduan", pengaduan);
+app.use("/gambar", gambar);
+app.use("/krisar", krisar);
 
-app.use("/roles",roles)
-app.use("/users",users)
-app.use("/kategori-pengaduan",katPengaduan)
-app.use("/pengaduan",pengaduan)
-app.use("/gambar",gambar)
-app.use('/krisar',krisar)
-
-app.listen(8080,function(){
-    console.log("server is listening")
-})
+app.listen(8080, function () {
+  console.log("server is listening");
+});
